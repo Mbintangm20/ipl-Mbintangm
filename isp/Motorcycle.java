9a1359fdba8a8e9a27be02345e5e54efbbd36038
Bintang\Admin <mbintangmandala237956@gmail.com>
2023-10-25 10:41:40 +0700

    perubahan isp

--- isp/Motorcycle.java
+++ isp/Motorcycle.java
@@ -12,9 +12,5 @@ public class Motorcycle implements VehicleInterface {
     @Overide
     public void refuel(){
 
-    }// this method can't be implemented karena motor tidak punya pintu
-    @Overide
-    public void openDoors(){
-
     }
 }
