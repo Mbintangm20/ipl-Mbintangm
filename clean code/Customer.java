abb837e982bc5ca08820a3ccf77e6839d6501a43
Bintang\Admin <mbintangmandala237956@gmail.com>
2023-11-28 22:38:59 +0700

    perubahan customer

--- clean code/Customer.java
+++ clean code/Customer.java
@@ -1,5 +1,5 @@
-public class DtaRcrd102 {
-    class DtaRcrd102 {
+public class Customer {
+    class Customer {
         private Date generationTimestamp;
         private Date modificationTimestamp;
         private final String recordId = "102";
