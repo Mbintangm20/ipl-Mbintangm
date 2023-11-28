e9438679ed71ce5614428d1122464d7e25825010
Bintang\Admin <mbintangmandala237956@gmail.com>
2023-11-28 22:37:51 +0700

    membuat customer

---
+++ clean code/Customer.java
@@ -0,0 +1,7 @@
+public class DtaRcrd102 {
+    class DtaRcrd102 {
+        private Date generationTimestamp;
+        private Date modificationTimestamp;
+        private final String recordId = "102";
+    }
+}
