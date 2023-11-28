3b791fd485d6859af1455462d16ff0e4ef4cc685
Bintang\Admin <mbintangmandala237956@gmail.com>
2023-11-28 23:01:59 +0700

    membuat comments

---
+++ clean code/GoodComments.java
@@ -0,0 +1,17 @@
+//legal
+// Copyright (C) 2011 by Osoco. All rights reserved.
+// Released under the terms of the GNU General Public License
+// version 2 or later.
+
+//informative
+// Returns an instance of the Responder being tested.
+protected abstract Responder responderInstance();
+// renaming the function: responderBeingTested
+// format matched kk:mm:ss EEE, MMM dd, yyyy
+Pattern timeMatcher = Pattern.compile("\\d*:\\d*:\\d* \\w*, \\w* \\d*,
+\\d*");
+
+//clarification
+// Copyright (C) 2011 by Osoco. All rights reserved.
+// Released under the terms of the GNU General Public License
+// version 2 or later.
