38fa1f2ca7159a9ade15f2218674df439a3ba038
Bintang\Admin <mbintangmandala237956@gmail.com>
2023-11-28 22:42:59 +0700

    membuat employee

---
+++ clean code/EmployeeType.java
@@ -0,0 +1,15 @@
+public class Employee {
+    class Employee {
+        abstract int payAmount(Employee emp);
+    }
+        class Salesman {
+        int payAmount(Employee emp) {
+        return emp.getMonthlySalary() + emp.getCommission();
+        }
+    }
+        class Manager {
+        int payAmount(Employee emp) {
+        return emp.getMonthlySalary() + emp.getBonus();
+        }
+    }
+}
